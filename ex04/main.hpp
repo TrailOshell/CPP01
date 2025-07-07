5d53918920dae7174c1076c80a110773a2d16bb2
TrailOshell <TrailOshell.com>
2025-07-08 01:45:11 +0700

    ex04 init

---
+++ ex04/main.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomchan <tsomchan@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 01:41:13 by tsomchan          #+#    #+#             */
+/*   Updated: 2025/07/08 01:42:04 by tsomchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_HPP
+# define MAIN_HPP
+# include <iostream>
+# include <string>
+
+# ifndef DEBUG_MODE
+#  define DEBUG_MODE	0
+# endif
+
+# define NCL		"\033[0m"
+# define BLK		"\033[0;30m"
+# define RED		"\033[0;31m"
+# define GRN		"\033[0;32m"
+# define YLW		"\033[0;33m"
+# define BLU		"\033[0;34m"
+# define PUR		"\033[0;35m"
+# define CYN		"\033[0;36m"
+# define WHT		"\033[0;37m"
+
+#endif
