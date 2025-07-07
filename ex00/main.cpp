8a25f9a622a041105e574b894e4b50f0b2116cd8
TrailOshell <TrailOshell.com>
2025-07-07 23:19:23 +0700

    ex00 edit debug

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,40 +1,28 @@
 #include "Zombie.hpp"
 
-std::string	start_div(void)
+void print_div(std::string text)
 {
-	return (YLW "- -- --- " NCL);
-}
-
-std::string	end_div(void)
-{
-	return (YLW " --- -- -" NCL);
-}
-
-std::string print_div(std::string text)
-{
-	return (start_div() + text + end_div());
+	std::cout << std::endl;
+	std::cout <<  YLW "- -- --- " NCL + text + YLW " --- -- -" NCL;
+	std::cout << std::endl;
 }
 
 
 int main(void)
 {
-	std::cout << print_div(CYN "CPP01 ex00") << std::endl;
-	std::cout << print_div(GRN "Begin the program") << std::endl;
-
-	std::cout << std::endl;
+	print_div(CYN "CPP01 ex00");
+	print_div(GRN "Program start");
 	
-	std::cout << print_div(GRN "Using newZombie()") << std::endl;
+	print_div(GRN "newZombie()");
 	Zombie *z = newZombie("Fresh new rotten zombie");
+
+	print_div(GRN "announce()");
 	z->announce();
 	delete z;
 
-	std::cout << std::endl;
-	
-	std::cout << print_div(YLW "Using randomChump()") << std::endl;
+	print_div(YLW "randomChump()");
 	randomChump("Random chumping zombie");
 
-	std::cout << std::endl;
-	
-	std::cout << print_div(PUR "  End of program   ") << std::endl;
+	print_div(RED "Program ended");
 	return 0;
 }
