a6807ab9e1246ca55106f52d955b855888869c66
TrailOshell <TrailOshell.com>
2025-07-07 17:44:39 +0700

    ex00 colored

---
+++ Style.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Style.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomchan <tsomchan@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 19:07:21 by tsomchan          #+#    #+#             */
+/*   Updated: 2025/05/21 15:27:51 by tsomchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	STYLE_HPP
+# define STYLE_HPP
+
+# define NCL		"\033[0m"
+# define BLK		"\033[0;30m"
+# define RED		"\033[0;31m"
+# define GRN		"\033[0;32m"
+# define YLW		"\033[0;33m"
+# define BLU		"\033[0;34m"
+# define PUR		"\033[0;35m"
+# define CYN		"\033[0;36m"
+# define WHT		"\033[0;37m"
+
+#endif
--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -20,4 +20,14 @@ Zombie* newZombie(std::string name);
 
 void randomChump(std::string name);
 
+# define NCL		"\033[0m"
+# define BLK		"\033[0;30m"
+# define RED		"\033[0;31m"
+# define GRN		"\033[0;32m"
+# define YLW		"\033[0;33m"
+# define BLU		"\033[0;34m"
+# define PUR		"\033[0;35m"
+# define CYN		"\033[0;36m"
+# define WHT		"\033[0;37m"
+
 #endif
