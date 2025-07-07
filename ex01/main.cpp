fb8f6531973c15d391cc1020b218723ca62f05e2
TrailOshell <TrailOshell.com>
2025-07-07 18:56:35 +0700

    edit main printing

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,12 +2,12 @@
 
 std::string	start_div(void)
 {
-	return (YLW "- -- ---" NCL);
+	return (YLW "- -- --- " NCL);
 }
 
 std::string	end_div(void)
 {
-	return (YLW "--- -- -" NCL);
+	return (YLW " --- -- -" NCL);
 }
 
 std::string run_div(std::string text)
@@ -19,18 +19,34 @@ int main(void)
 {
 	std::cout << run_div(GRN "Begin the program") << std::endl;
 
-	std::cout << GRN << "The hoard is coming!" << end_div() << std::endl;
+	std::cout << std::endl;
+
+	std::cout << run_div(GRN "The hoard is coming!") << std::endl;
 	Zombie *z = zombieHorde(3, "hoardling");
-	std::cout << YLW << "The hoard annouce itself" << end_div() << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << run_div(YLW "The hoard annouce itself") << std::endl;
 	z->announce();
-	std::cout << YLW << "The hoardlings annouce themself" << end_div() << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << run_div(YLW "The hoardlings annouce themself") << std::endl;
 	z[0].announce();
 	z[1].announce();
 	z[2].announce();
-	std::cout << YLW << "Unnamed hoardling tried to announce itself" << end_div() << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << run_div(YLW "Unnamed hoardling tried to announce itself") << std::endl;
 	z[10].announce();
-	std::cout << RED << "delete[]" << end_div() << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << run_div(RED "delete[]") << std::endl;
 	delete[] z;
+
+	std::cout << std::endl;
 
 	std::cout << run_div(PUR "  End of program   ") << std::endl;
 	return 0;
