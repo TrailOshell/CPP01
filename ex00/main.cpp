e0437957bc3c3101a34700e99b33e44ad848f451
TrailOshell <TrailOshell.com>
2025-07-07 17:45:23 +0700

    ex00 spacing the code

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,14 +3,17 @@
 int main(void)
 {
 	std::cout << YLW << "- -- ---" << CYN << " Begin the program " << YLW << "--- -- -" << NCL << std::endl;
+
 	std::cout << GRN;
 	Zombie *z = newZombie("Fresh new rotten zombie");
 	z->announce();
 	delete z;
 	std::cout << NCL;
+
 	std::cout << YLW;
 	randomChump("Random chumping zombie");
 	std::cout << NCL;
+
 	std::cout << YLW << "- -- ---" << PUR << "  End of program   " << YLW << "--- -- -" << NCL << std::endl;
 	return 0;
 }
