92462f993fbb56eecf5f681fdf313e3d631d2de9
TrailOshell <TrailOshell.com>
2025-07-08 04:36:10 +0700

    ex05 init

---
+++ ex05/Harl.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomchan <tsomchan@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 04:26:25 by tsomchan          #+#    #+#             */
+/*   Updated: 2025/07/08 04:35:23 by tsomchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+}
+
+Harl::~Harl()
+{
+}
+
+void	Harl::debug(void)
+{
+	std::cout << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << std::endl;
+}
+
+void	Harl::complain( std::string level )
+{
+}
