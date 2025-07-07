2367d4e28b322229f92b338e4b62af7622478774
TrailOshell <TrailOshell.com>
2025-07-07 23:55:19 +0700

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+	std::string	NCL = "\033[0m";
+	std::string	YLW = "\033[0;33m";
+	std::string	CYN = "\033[0;36m";
+
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << CYN << "memory addresses" << NCL << std::endl;
+	std::cout << YLW << "&str       -> " << CYN << &str       << NCL << std::endl;
+	std::cout << YLW << "&stringPTR -> " << CYN << &stringPTR << NCL << std::endl;
+	std::cout << YLW << "&stringREF -> " << CYN << &stringREF << NCL << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << CYN << "values" << NCL << std::endl;
+	std::cout << YLW << "str         = " << CYN << str       << NCL << std::endl;
+	std::cout << YLW << "stringPTR   = " << CYN << stringPTR << NCL << std::endl;
+	std::cout << YLW << "stringREF   = " << CYN << stringREF << NCL << std::endl;
+	return (0);
+}
