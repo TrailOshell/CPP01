672540e2446d7fc49855cd531a54fa7db3500294
TrailOshell <TrailOshell.com>
2025-07-11 23:45:08 +0700

    ex02 print edit

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tsomchan <tsomchan@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 00:06:23 by tsomchan          #+#    #+#             */
-/*   Updated: 2025/07/08 00:06:24 by tsomchan         ###   ########.fr       */
+/*   Updated: 2025/07/11 23:40:51 by tsomchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,16 @@ int main()
 	std::string *stringPTR = &str;
 	std::string &stringREF = str;
 
-	std::cout << CYN << "memory addresses" << NCL << std::endl;
-	std::cout << YLW << "&str       -> " << CYN << &str       << NCL << std::endl;
-	std::cout << YLW << "&stringPTR -> " << CYN << &stringPTR << NCL << std::endl;
-	std::cout << YLW << "&stringREF -> " << CYN << &stringREF << NCL << std::endl;
+	std::cout << CYN + "memory addresses" + NCL << std::endl;
+	std::cout << YLW + "&str       -> " << CYN << &str       << NCL << std::endl;
+	std::cout << YLW + "&stringPTR -> " << CYN << &stringPTR << NCL << std::endl;
+	std::cout << YLW + "&stringREF -> " << CYN << &stringREF << NCL << std::endl;
 
 	std::cout << std::endl;
 
-	std::cout << CYN << "values" << NCL << std::endl;
-	std::cout << YLW << "str         = " << CYN << str       << NCL << std::endl;
-	std::cout << YLW << "stringPTR   = " << CYN << stringPTR << NCL << std::endl;
-	std::cout << YLW << "stringREF   = " << CYN << stringREF << NCL << std::endl;
+	std::cout << CYN + "values" + NCL << std::endl;
+	std::cout << YLW + "str         = " << CYN << str       << NCL << std::endl;
+	std::cout << YLW + "stringPTR   = " << CYN << stringPTR << NCL << std::endl;
+	std::cout << YLW + "stringREF   = " << CYN << stringREF << NCL << std::endl;
 	return (0);
 }
